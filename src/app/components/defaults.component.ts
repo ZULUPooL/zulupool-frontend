3fa71a7bdd645c5f9e8e6d4ff45de1ae73fab619
EternityTula <eternity.tula@gmail.com>
2020-12-09 22:52:34 +0300

    x

--- src/app/components/defaults.component.ts
+++ src/app/components/defaults.component.ts
@@ -11,7 +11,7 @@ import {
 } from 'interfaces/common';
 
 export class DefaultParams {
-    static readonly GUIVERSION: string = 'v1.19b';
+    static readonly GUIVERSION: string = 'v1.20b';
 
     static readonly SUPPORTMAIL = '';
     static readonly DISCORDSERVER = '';
