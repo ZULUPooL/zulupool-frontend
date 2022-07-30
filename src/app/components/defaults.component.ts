a70cd76eb7bb12231ac3fe5316f55446a779a7cf
EternityTula <eternity.tula@gmail.com>
2022-07-31 00:34:59 +0300

    A typo correction

--- src/app/components/defaults.component.ts
+++ src/app/components/defaults.component.ts
@@ -4,7 +4,7 @@ import { ETime } from 'enums/time';
 export class DefaultParams {
     static readonly DNSNAME = 'zulupool.com';
 
-    static readonly GUIVERSION = 'v2.16b';
+    static readonly GUIVERSION = 'v2.17b';
     static readonly GUISOURCE = 'https://github.com/';
 
     static readonly COREVERSION = 'v0.9999b';
